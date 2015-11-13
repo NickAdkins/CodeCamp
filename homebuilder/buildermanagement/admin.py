2f9bc671fb65c61f61736854e3069f06cbe9b0fb
celestecarter95@gmail.com <ccarte39@dmail.dixie.edu>
2015-11-13 11:31:39 -0700

    Registered each class model in admin.py

--- homebuilder/buildermanagement/admin.py
+++ homebuilder/buildermanagement/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
+from .models import Group, Category, Room, Contact, Item, AddOn, Project, Phase
+
 # Register your models here.
+
+admin.site.register(Group)
+admin.site.register(Category)
+admin.site.register(Room)
+admin.site.register(Contact)
+admin.site.register(Item)
+admin.site.register(AddOn)
+admin.site.register(Project)
+admin.site.register(Phase)
