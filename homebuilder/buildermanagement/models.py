1bd29a7e28dc043c2f81938c584853007f2275e9
celestecarter95@gmail.com <ccarte39@dmail.dixie.edu>
2015-11-13 09:55:08 -0700

    Added Phase in models and the base.html

--- homebuilder/buildermanagement/models.py
+++ homebuilder/buildermanagement/models.py
@@ -3,3 +3,15 @@ from django.db import models
 # Create your models here.
 class Project(models.Model):
     buyer = models.ForeignKey(Contact)
+
+class Phase(models.Model):
+    start_date = models.DateTimeField()
+    end_date = models.DateTimeField()
+    completed = models.BooleanField(default=False)
+    name = models.CharField(max_length=200)
+    order_by = models.DateTimeField()
+    contact = ForeignKey(Contact)
+    depends_on = ForeignKey('Phase', blank=True, null=True)
+
+    def __unicode__(self):
+        return self.name
