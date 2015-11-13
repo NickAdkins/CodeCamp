054265b331cfd724dfc0b0f50ffb1c9cad89341d
celestecarter95@gmail.com <ccarte39@dmail.dixie.edu>
2015-11-13 13:54:56 -0700

    Added basic LDCUD for Project, Contact, Item

--- homebuilder/buildermanagement/urls.py
+++ homebuilder/buildermanagement/urls.py
@@ -4,4 +4,23 @@ from . import views
 
 urlpatterns = [
     url('groups/$', views.GroupListView.as_view(), name="group_list"),
+
+    url('contacts/$', views.ContactListView.as_view(), name="contact_list"),
+    url('contacts/(?P<pk>\d+)/$', login_required(views.ContactDetailView.as_view()), name="contact_detail"),
+    url('contacts/create/$', login_required(views.ContactCreateView.as_view()), name="contact_create"),
+    url('contacts/(?P<pk>\d+)/update/$', login_required(views.ContactUpdateView.as_view()), name="contact_update"),
+    url('contacts/(?P<pk>\d+)/delete/$', login_required(views.ContactDeleteView.as_view()), name="contact_delete"),
+
+    url('projects/$', views.ProjectListView.as_view(), name="project_list"),
+    url('projects/(?P<pk>\d+)/$', login_required(views.ProjectDetailView.as_view()), name="project_detail"),
+    url('projects/create/$', login_required(views.ProjectCreateView.as_view()), name="project_create"),
+    url('projects/(?P<pk>\d+)/update/$', login_required(views.ProjectUpdateView.as_view()), name="project_update"),
+    url('projects/(?P<pk>\d+)/delete/$', login_required(views.ProjectDeleteView.as_view()), name="project_delete"),
+
+    url('Items/$', views.ItemListView.as_view(), name="item_list"),
+    url('Items/(?P<pk>\d+)/$', login_required(views.ItemDetailView.as_view()), name="item_detail"),
+    url('Items/create/$', login_required(views.ItemCreateView.as_view()), name="item_create"),
+    url('Items/(?P<pk>\d+)/update/$', login_required(views.ItemUpdateView.as_view()), name="item_update"),
+    url('Items/(?P<pk>\d+)/delete/$', login_required(views.ItemDeleteView.as_view()), name="item_delete"),
+
 ]
--- homebuilder/buildermanagement/views.py
+++ homebuilder/buildermanagement/views.py
@@ -7,4 +7,71 @@ from .models import Project, Group, Contact, Room, Category, Item, AddOn
 class GroupListView(ListView):
     model = Group
 
+# Contact Views #
 
+class ContactListView(ListView):
+    models = Contact
+
+#class ContactDetailView(DetailView):
+#    models Contact
+#
+#class ContactCreateView(CreateView):
+#    models Contact
+#    success_url = reverse_lazy('bm:contact_list.html')
+#    success_message = "%(name)s was created successfully"
+#
+#class ContactUpdateView(UpdateView):
+#    models Contact
+#    success_url = reverse_lazy('bm:contact_list.html')
+#    success_message = "%(name)s was created successfully"
+#
+#class ContactDeleteView(DeleteView):
+#    models Contact
+#    success_url = reverse_lazy('bm:contact_list.html')
+#    success_message = "%(name)s was created successfully"
+#
+## Project Views #
+#
+#class ProjectListView(ListView):
+#    models = Project
+#
+#class ProjectDetailView(DetailView):
+#    models Project
+#
+#class ProjectCreateView(CreateView):
+#    models Project
+#    success_url = reverse_lazy('bm:project_list.html')
+#    success_message = "%(name)s was created successfully"
+#
+#class ProjectUpdateView(UpdateView):
+#    models Project
+#    success_url = reverse_lazy('bm:project_list.html')
+#    success_message = "%(name)s was created successfully"
+#
+#class ProjectDeleteView(DeleteView):
+#    models Project
+#    success_url = reverse_lazy('bm:project_list.html')
+#    success_message = "%(name)s was created successfully"
+#
+## Item Views #
+#
+#class ItemListView(ListView):
+#    models = Item
+#
+#class ItemDetailView(DetailView):
+#    models Item
+#
+#class ItemCreateView(CreateView):
+#    models Item
+#    success_url = reverse_lazy('bm:item_list.html')
+#    success_message = "%(name)s was created successfully"
+#
+#class ItemUpdateView(UpdateView):
+#    models Item
+#    success_url = reverse_lazy('bm:item_list.html')
+#    success_message = "%(name)s was updated successfully"
+#
+#class ItemDeleteView(DeleteView):
+#    models Item
+#    success_url = reverse_lazy('bm:item_list.html')
+#    success_message = "%(name)s was deleted successfully"
