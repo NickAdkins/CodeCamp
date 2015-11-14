12f916d9d903669a6035462b5eb2dd6785866871
celestecarter95@gmail.com <ccarte39@dmail.dixie.edu>
2015-11-13 21:42:44 -0700

    Fixed Phase models to make the slug. Also still working on the calender.

---
+++ homebuilder/buildermanagement/migrations/0011_merge.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('buildermanagement', '0010_phase_slug'),
+        ('buildermanagement', '0006_auto_20151113_1939'),
+        ('buildermanagement', '0009_merge'),
+    ]
+
+    operations = [
+    ]
